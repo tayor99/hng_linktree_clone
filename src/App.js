9848f6f9a985b2d0b8ec35f59bd5ae7be641e435
tayor <adenusiadetayo@gmail.com>
2022-10-31 14:15:01 +0100

    Created a profile header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import profileImg from "./Assets/tayo.jpg";
 function App() {
-  return <h1>Hiii</h1>;
+  return (
+    <div className="app">
+      <header>
+        <div className="profile">
+          <div className="profile__image">
+            <img src={profileImg} alt="profileImg" />
+          </div>
+          <div className="profile__details">
+            <p id="tweeterName">Tayor_Official</p>
+            <p id="slack">Tayor99</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
